9d8f3fddbbf4bafb9ebbdf5c66cf68f7525ac8ad
Manuel Bogner <manuel.bogner@openresearch.com>
2017-11-05 02:17:09 +0100

    some empty test

---
+++ api/src/test/java/pm/mbo/api/controller/FootControllerTest.java
@@ -0,0 +1,13 @@
+package pm.mbo.api.controller;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class FootControllerTest {
+
+	@Test
+	public void get() throws Exception {
+	}
+
+}
